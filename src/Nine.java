b2ad630f64b48b18470032a1517c0245d2a94bab
eumenides <eumescan@qq.com>
2018-07-19 21:00:05 +0800

    1-7-9

---
+++ src/Nine.java
@@ -0,0 +1,22 @@
+/**
+ * @author eumes
+ * @date 2018/7/19
+ */
+public class Nine {
+
+    public static void main(String[] args){
+        System.out.println(isPalindrome(1010));
+    }
+
+    public static boolean isPalindrome(int x){
+        int y = 0;
+        if(x < 0 || (x % 10 == 0 && x != 0))
+            return false;
+        while (y < x) {
+            int a = x % 10;
+            y = y * 10 + a;
+            x /= 10;
+        }
+        return x == y || x == y / 10;
+    }
+}
---
+++ src/Seven.java
@@ -0,0 +1,27 @@
+/**
+ * @author eumes
+ * @date 2018/7/19
+ */
+public class Seven {
+    public static void main(String[] args){
+
+        System.out.println(reverse(-324));
+    }
+
+    public static int reverse(int x) {
+        long result = 0l;
+
+        while (x != 0) {
+            int a = x % 10;
+            x  /= 10;
+//            if(result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && a > 7))
+//                return 0;
+//            if(result < Integer.MIN_VALUE /10 || (result == Integer.MIN_VALUE / 10 && a < -8))
+//                return 0;
+            result = result * 10 + a;
+        }
+        if(result < Integer.MIN_VALUE || result > Integer.MAX_VALUE)
+            return 0;
+        return (int)result;
+    }
+}
