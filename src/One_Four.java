8fb044d473c9d67c9457b6e33d1e7ae4c40e25ab
eumenides <eumescan@qq.com>
2018-07-24 00:41:39 +0800

    13-14

---
+++ src/One_Four.java
@@ -0,0 +1,30 @@
+/**
+ * @author eumes
+ * @date 2018/7/24
+ */
+public class One_Four {
+
+    public static void main(String[] args){
+        String[] strings = {"flower", "flow", "flight"};
+
+        System.out.println(longestCommonPrefix(strings));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || 0 == strs.length) {
+            return "";
+        }
+        String prefix = strs[0];
+        for (String s:strs ) {
+            if (prefix == s) {
+                continue;
+            }
+            for(int i = 0; i < prefix.length(); ++i) {
+                if (i == s.length() || s.charAt(i) != prefix.charAt(i)) {
+                    prefix = prefix.substring(0, i);
+                }
+            }
+        }
+        return prefix;
+    }
+}
---
+++ src/One_Three.java
@@ -0,0 +1,45 @@
+import java.util.HashMap;
+
+/**
+ * @author eumes
+ * @date 2018/7/23
+ */
+public class One_Three {
+
+    public static void main(String[] args){
+
+        String s = "MCMXCIV";
+
+        System.out.println(romanToInt1(s));
+    }
+
+    public static int romanToInt1(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int result = 0;
+        int i = 0;
+        for(; i < s.length() - 1; ++i){
+            int a = map.get(s.charAt(i));
+            int b = map.get(s.charAt(i + 1));
+
+            if (a < b) {
+                result += b - a;
+                i++;
+            }else{
+                result += a;
+            }
+        }
+        if (i == s.length() - 1) {
+            return result + map.get(s.charAt(i));
+        }else {
+            return result;
+        }
+    }
+}
